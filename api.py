943ebc07ddded91b9357f81145108de1006ff3d2
dessyd <reprendre.mezzo_0j@icloud.com>
2021-11-21 15:04:19 +0100

    double single quotes

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ def get_api_key(section: str):
     config = configparser.ConfigParser()
     config.read('apis.conf')
     try:
-        api_key = config.get(section, "api")
+        api_key = config.get(section, 'api')
     except (configparser.NoSectionError, configparser.NoOptionError):
         api_key = None
     return api_key
@@ -13,7 +13,7 @@ def get_api_endpoint(section: str):
     config = configparser.ConfigParser()
     config.read('endpoints.conf')
     try:
-        endpoint = config.get(section, "endpoint")
+        endpoint = config.get(section, 'endpoint')
     except (configparser.NoSectionError, configparser.NoOptionError):
         endpoint = None
     return endpoint
