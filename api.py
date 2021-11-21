078a03b995b0aacf527a33313cf037b44851b83b
dessyd <reprendre.mezzo_0j@icloud.com>
2021-11-21 15:02:52 +0100

    Error Handling

--- api.py
+++ api.py
@@ -3,11 +3,17 @@ import configparser
 def get_api_key(section: str):
     config = configparser.ConfigParser()
     config.read('apis.conf')
-    api_key = config[section]["api"]
+    try:
+        api_key = config.get(section, "api")
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        api_key = None
     return api_key
 
 def get_api_endpoint(section: str):
     config = configparser.ConfigParser()
     config.read('endpoints.conf')
-    endpoint = config[section]["endpoint"]
+    try:
+        endpoint = config.get(section, "endpoint")
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        endpoint = None
     return endpoint
